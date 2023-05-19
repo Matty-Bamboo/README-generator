42b30bc3b7ded01814e74dc1ea5c1dc8643d2108
Matt Miceli <126421060+Matty-Bamboo@users.noreply.github.com>
2023-05-18 18:10:20 -0700

    updated files

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,19 +1,66 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
+    case 'Apache':
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]';
+    case 'GNU':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]';
+    case 'BSD 3-Clause':
+      return '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]';
+    case 'BSD 2-Clause':
+      return '[![License](https://img.shields.io/badge/License-BSD%202--Clause-blue.svg)]';
+      default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'Apache 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'GNU GPLv3':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    case 'BSD 3-Clause':
+      return 'https://opensource.org/licenses/BSD-3-Clause';
+    case 'BSK 2-Clause':
+      return 'https://opensource.org/licenses/BSD-2-Clause';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    const licenseBadge = renderLicenseBadge(license);
+    const licenseLink = renderLicenseLink(license);
+
+    return `
+## License
+
+${licenseBadge}
+
+This application is covered under the [${license} License](${licenseLink}).
+`;
+  }
+
+  return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+##License
+${renderLicenseSection(data.license)}
 `;
 }
 
--- index.js
+++ index.js
@@ -1,13 +1,68 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require("./generateMarkdown");
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Description'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Contributing'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Tests'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application',
+        choices: ['None', 'Apache', 'GNU', 'MIT', 'BSD 2-Clause', 'BSD 3-Clause'],
+    },
+]
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+
+// TODO: Create a function to write README file (fs code to save file)
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(`README file "${fileName}" created successfully!`);
+      });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function(answers) {
+        const markdownText = generateMarkdown(answers);
+        writeToFile('README1.md', markdownText);
+    });
+}
 
 // Function call to initialize app
 init();
